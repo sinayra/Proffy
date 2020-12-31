93d0605e7b9170c2a185e7c8f663838b2b8673d0
sinayra <nayra@hotmail.co.jp>
2020-12-31 15:16:01 +0100

    Added query description

--- api/schema/query.js
+++ api/schema/query.js
@@ -1,9 +1,14 @@
 module.exports = `
 type Query {
+    "list of users"
     users: [User]
+    "list of teachers"
     teachers(teacher: TeacherInput): [Teacher]
+    "list of students"
     students: [Student]
+    "get teacher from the current user after them have logged in. requires a teacher role account"
     teacherByCurrentUser: Teacher @requiresRole(role: "TEACHER")
+    "get student from the current user after them have logged in.  requires a student role account"
     studentByCurrentUser: Student @requiresRole(role: "STUDENT")
 }
 `;
